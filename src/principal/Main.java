74d7eb59af08f3610eea4bd78d574a7eb168faba
junionob <junio.waldomiro@gmail.com>
2023-04-27 15:52:52 -0300

    "1.0"

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,2 +1,16 @@
+package principal;
+
 public class Main {
     public Estoque material;
+    Cliente titular;
+
+    public boolean vender(int disponibilidade){
+        if (this.disponibilidade >= 1) {
+            System.out.println("Disponivel em estoque: " + this.disponibilidade + this.volume);
+            return true;
+        } else {
+            System.out.println("Sem estoque");
+        }
+    }
+
+}
--- src/principal/TestaVenda.java
+++ src/principal/TestaVenda.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TestaVenda {
+package principal;
+
+public class TestaVenda {
+    public static void main(String[] args) {
+        Estoque produto = new Estoque();
+        produto.departamento = "pintura";
+        produto.tipoMarca = "Lata de tinta";
+        produto.volume = "1L";
+        produto.setValor(50);
+        produto.disponibilidade = 10;
+
+
+    }
 }
